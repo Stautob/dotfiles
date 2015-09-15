1b2593dc305dd08d85b2a2927f5ad4908e39c673
Tobias Stauber <tstauber@hsr.ch>
2015-09-15 12:24:30 +0000

    corrected android script

--- androidstudio.sh
+++ androidstudio.sh
@@ -10,12 +10,13 @@ sudo pacman -S --noconfirm lib32-mesa lib32-libxrender lib32-fontconfig
 curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
 tar -xvzf package-query.tar.gz
 cd package-query
-sudo makepkg -si
-
+makepkg -si
 cd ..
+
 curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
 tar -xvzf yaourt.tar.gz
 cd yaourt
-sudo makepkg -si
+makepkg -si
+cd ..
 
-sudo yaourt -S --noconfirm android-studio
+sudo yaourt -S --noconfirm android-studio android-tools
