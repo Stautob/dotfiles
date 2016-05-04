85e4eed7b90dddf0f34ae8478f5f6b3e6ded579d
Stautob <stautob@stautob.ch>
2016-05-04 23:31:53 +0200

    Added colors for man and some xrandr functions

--- fish/functions.fish
+++ fish/functions.fish
@@ -74,6 +74,23 @@ end
 
 function pivotrandr
   xrandr --auto
-  #xrandr --output LVDS1 --auto --pos 1200x1020 --output VGA1 --rotate left --pos 0x0
   xrandr --output VGA1 --rotate left --mode 1920x1200 --pos 0x0 --output LVDS1 --auto --pos 1200x1021
 end
+
+function norandr
+  xrandr --auto
+  xrandr --output VGA1 --off
+end
+
+function fhdrandr
+  xrandr --auto
+  xrandr --output VGA1 --mode 1920x1080 --pos 0x0 --output LVDS1 --auto --pos 1921x180
+end
+
+set -xU LESS_TERMCAP_mb (printf "\e[01;31m")      # begin blinking
+set -xU LESS_TERMCAP_md (printf "\e[01;31m")      # begin bold
+set -xU LESS_TERMCAP_me (printf "\e[0m")          # end mode
+set -xU LESS_TERMCAP_se (printf "\e[0m")          # end standout-mode
+set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")   # begin standout-mode - info box
+set -xU LESS_TERMCAP_ue (printf "\e[0m")          # end underline
+set -xU LESS_TERMCAP_us (printf "\e[01;32m")      # begin underline
