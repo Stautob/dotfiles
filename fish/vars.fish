ef6769df7ba5484bae98aa57f55d0e66b3c9c77d
Stautob <tobias.stauber@hsr.ch>
2018-05-02 13:10:58 +0200

    Used termite to replace terminator

--- fish/vars.fish
+++ fish/vars.fish
@@ -6,7 +6,9 @@
 
 set -g fish_greeting ""
 set -g theme_display_user "yes"
+set -gx TERMINAL "termite"
 set -gx TERM "xterm-256color"
+set -gx SHELL "fish"
 set -gx editor "nvim"
 set -gx EDITOR "nvim"
 set -gx Editor "nvim"
