1a850f680ffe367a0109641c7b648e0137365722
Stautob <tobias.stauber@hsr.ch>
2018-05-11 18:52:07 +0200

    Added debug mechanism

--- fish/config.fish
+++ fish/config.fish
@@ -29,3 +29,30 @@ source {$ConfigDir}functions_display.fish
 
 # load keymanagement
 source {$ConfigDir}keymanagement.fish
+
+# Debug setup
+set debug_buffer ""
+set debug_key ""
+
+function fish_debug -a key line --on-event print_debug
+  echo "key: $key line: $line debug_key: $debug_key"
+  if test "$key" != "$debug_key"
+    if test -n "$debug_buffer[1]"
+      flush_fish_debug
+    end
+    set debug_key $key
+    set debug_buffer "-------------[$debug_key]--------------"
+  end
+  set debug_buffer $debug_buffer $line
+end
+
+function flush_fish_debug -a file --on-event flush_debug
+  if test -z $file
+    set file "/tmp/fish_debug.log"
+  end
+  for line in $debug_buffer
+    echo $line >> $file
+  end
+  set debug_buffer ""
+  set debug_key ""
+end
