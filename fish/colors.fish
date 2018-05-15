16b5e530e2e969b83d62a7c9f193b9750ffd5e1a
Stautob <tobias.stauber@hsr.ch>
2018-05-15 17:22:35 +0200

    Moved pager coloring to colors.fish

--- fish/colors.fish
+++ fish/colors.fish
@@ -4,3 +4,16 @@ set custom_color_medium_blue 0ABFFC
 set custom_color_yellow dfdf00
 set custom_color_orange f6b117
 set custom_color_red ce000f
+
+
+#---------------------#
+# Colorize applications
+
+# Colorize pager
+set -xU LESS_TERMCAP_mb (printf "\e[01;31m")      # begin blinking
+set -xU LESS_TERMCAP_md (printf "\e[01;31m")      # begin bold
+set -xU LESS_TERMCAP_me (printf "\e[0m")          # end mode
+set -xU LESS_TERMCAP_se (printf "\e[0m")          # end standout-mode
+set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")   # begin standout-mode - info box
+set -xU LESS_TERMCAP_ue (printf "\e[0m")          # end underline
+set -xU LESS_TERMCAP_us (printf "\e[01;32m")      # begin underline
