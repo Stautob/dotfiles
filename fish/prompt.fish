3c7dea0d6d662df6e1e0269c48e6e4689b983068
Stautob <tobias.stauber@hsr.ch>
2018-05-15 17:50:23 +0200

    Formatting

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -1,28 +1,28 @@
 #!/usr/bin/fish
 
 # Git glyphs
-set branch_glyph            ''
-set detached_glyph          "➦"
-set pull_pending_glyph      '⬇'
-set push_pending_glyph      '⇧'
+set branch_glyph ''
+set detached_glyph "➦"
+set pull_pending_glyph '⬇'
+set push_pending_glyph '⇧'
 
 # Other glyphs
-set prompt_finisher_glyph   '❱ '
+set prompt_finisher_glyph '❱ '
 
 
 # ===========================
 # String Helper methods
 # ===========================
 
-function __g2_color_print -a text color
-  set_color $color $argv[2..-1]
-  echo -ne $text
-  set_color normal
+function __g3_color_print -a text color
+    set_color $color $argv[2..-1]
+    echo -ne $text
+    set_color normal
 end
 
 function __g2_enclose_in_brackets
-  emit print_debug "prompt" "enclose_in_brackets| argv: $argv"
-  echo -ne "⦗"$argv"⦘"
+    emit print_debug "prompt" "enclose_in_brackets| argv: $argv"
+    echo -ne "⦗"$argv"⦘"
 end
 
 
@@ -31,7 +31,7 @@ end
 # ===========================
 
 function __g2_pretty_path -a path
-  echo -n (string replace "$HOME" "~" $path | string replace --all --regex "(?'short'\.?[^/])[^/]*/" "\$short/")
+    echo -n (string replace "$HOME" "~" $path | string replace --all --regex "(?'short'\.?[^/])[^/]*/" "\$short/")
 end
 
 
@@ -40,79 +40,81 @@ end
 # ===========================
 
 function __g2_prompt_project_dir -d 'Print the current git project base directory'
-  command git rev-parse --show-toplevel ^/dev/null
+    command git rev-parse --show-toplevel ^/dev/null
 end
 
 function __g2_prompt_getBranchOp
-  set -l base_dir
-  set -l git_dir (command git rev-parse --git-dir ^/dev/null)
-  test ! -d $git_dir; and return 1
+    set -l base_dir
+    set -l git_dir (command git rev-parse --git-dir ^/dev/null)
+    test ! -d $git_dir
+    and return 1
 
-  # get repo status & branch name
-  set -l op ''
-  set -l branch ''
+    # get repo status & branch name
+    set -l op ''
+    set -l branch ''
 
-  command git ls-tree HEAD > /dev/null ^ /dev/null
+    command git ls-tree HEAD >/dev/null ^/dev/null
 
-  if test $status -eq 128
-    set op 'init'
-  else
-    set -l step
-    set -l total
-
-    if test -d "$git_dir/rebase-merge"
-      set step (cat "$git_dir/rebase-merge/msgnum")
-      set total (cat "$git_dir/rebase-merge/end")
-      set branch (cat "$git_dir/rebase-merge/head-name")" $step/$total"
-
-      if test -f "$git_dir/rebase-merge/interactive"
-        set op 'rebase -i'
-      else
-        set op 'rebase -m'
-      end
+    if test $status -eq 128
+        set op 'init'
     else
-      if test -d "$git_dir/rebase-apply"
-        set step (cat "$git_dir/rebase-apply/next")
-        set total (cat "$git_dir/rebase-apply/last")
+        set -l step
+        set -l total
 
-        if test -f "$git_dir/rebase-apply/rebasing"
-          set op 'rebase'
-        else if test -f "$git_dir/rebase-apply/applying"
-          set op 'am'
-        else
-          set op 'am/rebase'
-        end
-      else
-        if test -f "$git_dir/MERGE_HEAD"
-          set op 'merge'
-        else if test -f "$git_dir/CHERRY_PICK_HEAD"
-          set op 'cherrypick'
-        else if test -f "$git_dir/REVERT_HEAD"
-          set op 'revert'
-        else if test -f "$git_dir/BISECT_LOG"
-          set op 'bisect'
-        end
-      end
-      if not set branch (command git symbolic-ref HEAD ^/dev/null)
-        test ! "$op"; and set op 'detached'
-        if not set branch (command git describe --tags --exact-match HEAD ^/dev/null)
-          if not set branch (cut -c 1-7 "$git_dir/HEAD" ^/dev/null)
-            set branch 'unknown'
-          end
-        end
+        if test -d "$git_dir/rebase-merge"
+            set step (cat "$git_dir/rebase-merge/msgnum")
+            set total (cat "$git_dir/rebase-merge/end")
+            set branch (cat "$git_dir/rebase-merge/head-name")" $step/$total"
 
-        if test "$step" -a "$total"
-          set branch "[$branch $step/$total]"
+            if test -f "$git_dir/rebase-merge/interactive"
+                set op 'rebase -i'
+            else
+                set op 'rebase -m'
+            end
         else
-          set branch "[$branch]"
+            if test -d "$git_dir/rebase-apply"
+                set step (cat "$git_dir/rebase-apply/next")
+                set total (cat "$git_dir/rebase-apply/last")
+
+                if test -f "$git_dir/rebase-apply/rebasing"
+                    set op 'rebase'
+                else if test -f "$git_dir/rebase-apply/applying"
+                    set op 'am'
+                else
+                    set op 'am/rebase'
+                end
+            else
+                if test -f "$git_dir/MERGE_HEAD"
+                    set op 'merge'
+                else if test -f "$git_dir/CHERRY_PICK_HEAD"
+                    set op 'cherrypick'
+                else if test -f "$git_dir/REVERT_HEAD"
+                    set op 'revert'
+                else if test -f "$git_dir/BISECT_LOG"
+                    set op 'bisect'
+                end
+            end
+            if not set branch (command git symbolic-ref HEAD ^/dev/null)
+                test ! "$op"
+                and set op 'detached'
+                if not set branch (command git describe --tags --exact-match HEAD ^/dev/null)
+                    if not set branch (cut -c 1-7 "$git_dir/HEAD" ^/dev/null)
+                        set branch 'unknown'
+                    end
+                end
+
+                if test "$step" -a "$total"
+                    set branch "[$branch $step/$total]"
+                else
+                    set branch "[$branch]"
+                end
+            end
         end
-      end
     end
-  end
 
-  emit print_debug "prompt" "getBranchOp| branch: $branch"
-  echo (string replace --all "refs/heads/" "" $branch)
-  echo $op
+    emit print_debug "prompt" "getBranchOp| branch: $branch"
+    echo (string replace --all "refs/heads/" "" $branch)
+    echo $op
 end
 
 
@@ -121,11 +123,11 @@ end
 # ===========================
 
 function __g2_prompt_path_segment -d 'Display a shortened form of a directory'
-  __g2_color_print (__g2_enclose_in_brackets (prompt_pwd))" " $custom_color_medium_blue
+    __g2_color_print (__g2_enclose_in_brackets (prompt_pwd))" " $custom_color_medium_blue
 end
 
 function __g2_prompt_finish_segments -d 'Close open segments'
-  __g2_color_print $prompt_finisher_glyph green --bold
+    __g2_color_print $prompt_finisher_glyph green --bold
 end
 
 
@@ -135,95 +137,106 @@ end
 
 
 function __g2_prompt_status -d 'the symbols for a non zero exit status, root and background jobs'
-  # Print non-zero exit status
-  if test $PROMPT_LAST_STATUS -ne 0
-    __g2_color_print (__g2_enclose_in_brackets "⚡") $custom_color_red --bold #--background red
-  end
+    # Print non-zero exit status
+    if test $PROMPT_LAST_STATUS -ne 0
+        __g2_color_print (__g2_enclose_in_brackets "⚡") $custom_color_red --bold #--background red
+    end
 
-  # Print super-user glyph
-  if test (id -u $USER) -eq 0
-    __g2_color_print (__g2_enclose_in_brackets "SU") red --bold
-  end
+    # Print super-user glyph
+    if test (id -u $USER) -eq 0
+        __g2_color_print (__g2_enclose_in_brackets "SU") red --bold
+    end
 
-  # Print background-jobs glyph
-  if test (jobs -l | wc -l) -gt 0
-    __g2_color_print (__g2_enclose_in_brackets "BG") $custom_color_yellow --bold
-  end
+    # Print background-jobs glyph
+    if test (jobs -l | wc -l) -gt 0
+        __g2_color_print (__g2_enclose_in_brackets "BG") $custom_color_yellow --bold
+    end
 end
 
 function __g2_prompt_user -d 'Display actual user if different from $default_user'
-  test "$theme_display_user" = "true"; and set -l user (whoami)
-  ILTIS_is_remote; and set -l host "@"(prompt_hostname)
-  test -n "$user" -a -n "$host"; echo -n (__g2_enclose_in_brackets $user$host)
+    test "$theme_display_user" = "true"
+    and set -l user (whoami)
+    ILTIS_is_remote
+    and set -l host "@"(prompt_hostname)
+    test -n "$user" -a -n "$host"
+    echo -n (__g2_enclose_in_brackets $user$host)
 end
 
 function __g2_getremote
-  set -l remote (command git rev-parse --symbolic-full-name --abbrev-ref '@{u}' ^/dev/null); and echo $remote
+    set -l remote (command git rev-parse --symbolic-full-name --abbrev-ref '@{u}' ^/dev/null)
+    and echo $remote
 end
 
 function __g2_prompt_aheadbehind -a local
-  string match -q "detached:*" $local; and return 1
-  set -l cnt (command git rev-list --left-right --count $local...(__g2_getremote) -- ^/dev/null |tr \t \n)
-  emit print_debug "prompt" "prompt_aheadbehing| local: $local cnt[1]: $cnt[1] cnt[2]: $cnt[2]"
-  if test $cnt[1] -gt 0 -a $cnt[2] -gt 0
-    echo -n '±'
-  else
-    test $cnt[1] -gt 0; and echo -n '+'
-    test $cnt[2] -gt 0; and echo -n '-'
-  end
+    string match -q "detached:*" $local
+    and return 1
+    set -l cnt (command git rev-list --left-right --count $local...(__g2_getremote) -- ^/dev/null |tr \t \n)
+    emit print_debug "prompt" "prompt_aheadbehing| local: $local cnt[1]: $cnt[1] cnt[2]: $cnt[2]"
+    if test $cnt[1] -gt 0 -a $cnt[2] -gt 0
+        echo -n '±'
+    else
+        test $cnt[1] -gt 0
+        and echo -n '+'
+        test $cnt[2] -gt 0
+        and echo -n '-'
+    end
 end
 
 function __g2_prompt_git -d 'Display the actual git state'
 
-  command git rev-parse --is-inside-work-tree > /dev/null ^ /dev/null; or return 1
+    command git rev-parse --is-inside-work-tree >/dev/null ^/dev/null
+    or return 1
 
-  set -l v (__g2_prompt_getBranchOp)
-  set -l branch $v[1]
-  set -l op $v[2]
+    set -l v (__g2_prompt_getBranchOp)
+    set -l branch $v[1]
+    set -l op $v[2]
 
-  set -l icon "$branch_glyph "
-  test "$op" = "detached"; and set icon "$detached_glyph "
+    set -l icon "$branch_glyph "
+    test "$op" = "detached"
+    and set icon "$detached_glyph "
 
-  #### PARSE STATUS
-  set -l new 0
-  set -l staged 0
-  set -l dirty 0
+    #### PARSE STATUS
+    set -l new 0
+    set -l staged 0
+    set -l dirty 0
 
-  set -l git_status (command git status --porcelain ^/dev/null)
+    set -l git_status (command git status --porcelain ^/dev/null)
 
-  for line in $git_status
-    set -l x (echo $line | cut -c 1)
-    set -l y (echo $line | cut -c 2)
+    for line in $git_status
+        set -l x (echo $line | cut -c 1)
+        set -l y (echo $line | cut -c 2)
 
-    if test $x = '?'
-        set new (math $new + 1)
-    else
-        test "$x" != ' '; and set staged (math $staged + 1)
-        test "$y" != ' '; and set dirty (math $dirty + 1)
+        if test $x = '?'
+            set new (math $new + 1)
+        else
+            test "$x" != ' '
+            and set staged (math $staged + 1)
+            test "$y" != ' '
+            and set dirty (math $dirty + 1)
+        end
     end
-  end
 
-  if test -n "$op"
-    if test "$op" = 'init'
-      set local_color_flag white
-      set branch $op
+    if test -n "$op"
+        if test "$op" = 'init'
+            set local_color_flag white
+            set branch $op
+        else
+            set local_color_flag $custom_color_red
+            set branch "$op:$branch"
+        end
+    else if test $dirty -gt 0 -o $new -gt 0
+        set local_color_flag $custom_color_orange
+    else if test $staged -gt 0
+        set local_color_flag green
     else
-      set local_color_flag $custom_color_red
-      set branch "$op:$branch"
+        set local_color_flag brblack
     end
-  else if test $dirty -gt 0 -o $new -gt 0
-    set local_color_flag $custom_color_orange
-  else if test $staged -gt 0
-    set local_color_flag green
-  else
-    set local_color_flag brblack
-  end
 
 
-  set -l ahead_behind (__g2_prompt_aheadbehind $branch)
+    set -l ahead_behind (__g2_prompt_aheadbehind $branch)
 
-  emit print_debug "prompt" "prompt_git| icon: $icon branch: $branch ahead_behind: $ahead_behind"
-  __g2_color_print (__g2_enclose_in_brackets "$icon $branch $ahead_behind") $local_color_flag --bold
+    emit print_debug "prompt" "prompt_git| icon: $icon branch: $branch ahead_behind: $ahead_behind"
+    __g2_color_print (__g2_enclose_in_brackets "$icon $branch $ahead_behind") $local_color_flag --bold
 end
 
 
@@ -233,43 +246,46 @@ end
 # ===========================
 
 function fish_prompt -d "Write out left part of prompt"
-  set -g PROMPT_LAST_STATUS $status
-  set -gx EXPORTEDPWD $PWD
+    set -g PROMPT_LAST_STATUS $status
+    set -gx EXPORTEDPWD $PWD
 
-  __g2_prompt_status
-  __g2_prompt_user
-  __g2_prompt_path_segment "$PWD"
-  __g2_prompt_git
-  __g2_prompt_finish_segments
+    __g2_prompt_status
+    __g2_prompt_user
+    __g2_prompt_path_segment "$PWD"
+    __g2_prompt_git
+    __g2_prompt_finish_segments
 
-  emit flush_fish_debug "/tmp/fish_prompt_debug.log"
+    emit flush_fish_debug "/tmp/fish_prompt_debug.log"
 end
 
 function fish_mode_prompt
-  switch $fish_bind_mode
-    case default
-      set_color --bold red
-      echo (__g2_enclose_in_brackets 'N')
-    case insert
-      set_color --bold green
-      echo (__g2_enclose_in_brackets 'I')
-    case replace_one
-      set_color --bold green
-      echo (__g2_enclose_in_brackets 'R')
-    case visual
-      set_color --bold brmagenta
-      echo (__g2_enclose_in_brackets 'V')
-    case '*'
-      set_color --bold red
-      echo (__g2_enclose_in_brackets '?')
-  end
-  echo " "; set_color normal
+    switch $fish_bind_mode
+        case default
+            set_color --bold red
+            echo (__g2_enclose_in_brackets 'N')
+        case insert
+            set_color --bold green
+            echo (__g2_enclose_in_brackets 'I')
+        case replace_one
+            set_color --bold green
+            echo (__g2_enclose_in_brackets 'R')
+        case visual
+            set_color --bold brmagenta
+            echo (__g2_enclose_in_brackets 'V')
+        case '*'
+            set_color --bold red
+            echo (__g2_enclose_in_brackets '?')
+    end
+    echo " "
+    set_color normal
 end
 
 function fish_right_prompt -d "Write out the right prompt"
-  if test "$dirstack[1]"
-    test (count $dirstack) -gt 1; and set more " +";
-    __g2_color_print (__g2_enclose_in_brackets (__g2_pretty_path "$dirstack[1]$more")) green --bold
-  end
+    if test "$dirstack[1]"
+        test (count $dirstack) -gt 1
+        and set more " +"
+
+        __g2_color_print (__g2_enclose_in_brackets (__g2_pretty_path "$dirstack[1]$more")) green --bold
+    end
 end
 
