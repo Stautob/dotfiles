12bb9db5babdc74389e1b137fd47934f57833102
Stautob <tobias.staubert@gmail.com>
2024-06-19 15:05:51 +0200

    Fix wrong function call in prompt

--- fish/synced_functions/fish_prompt.fish
+++ fish/synced_functions/fish_prompt.fish
@@ -61,7 +61,7 @@ function fish_prompt --description 'Write out left part of prompt'
                     set branch "$op:$branch"
                 end
             else if git-is-touched
-                or git-untracked-files >/dev/null
+                or git-list-untracked >/dev/null
                 set local_color_flag $custom_color_orange
             else if git-is-staged
                 set local_color_flag green
